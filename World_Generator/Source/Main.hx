480502baae34be364a4b5e9dff9e55ca72dcc39f
M. Gardner <magardner2010@gmail.com>
2020-07-12 13:32:54 +0300

    make it display a bitmap

--- World_Generator/Source/Main.hx
+++ World_Generator/Source/Main.hx
@@ -1,11 +1,19 @@
-package;
-
+import openfl.display.Bitmap;
+import openfl.display.BitmapData;
 import openfl.display.Sprite;
-
+import openfl.Lib;
 class Main extends Sprite
 {
 	public function new()
 	{
 		super();
+		BitmapData.loadFromFile ("assets/openfl.png").onComplete (function (bitmapData) {
+		    var bitmap = new Bitmap (bitmapData);
+            addChild (bitmap);
+        });
+	}
+
+	static function main(){
+	    Lib.current.addChild(new Main());
 	}
 }
