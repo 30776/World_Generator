e360a1fd7472a2ac8e8e3c10ee913220cc57cc8a
M. Gardner <magardner2010@gmail.com>
2020-07-12 14:31:33 +0300

    initial converter is done. And boy is the result ugly.

--- World_Generator/Source/Main.hx
+++ World_Generator/Source/Main.hx
@@ -7,13 +7,29 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		BitmapData.loadFromFile ("assets/openfl.png").onComplete (function (bitmapData) {
+		BitmapData.loadFromFile ("assets/test.png").onComplete (function (bitmapData) {
 		    var bitmap = new Bitmap (bitmapData);
             addChild (bitmap);
+            addChild (new Bitmap(analyse(bitmap.bitmapData)));
         });
 	}
 
 	static function main(){
 	    Lib.current.addChild(new Main());
+	}
+
+	public function analyse( image:BitmapData ):BitmapData{
+	    var ret=image.clone();
+	    ret.lock();
+        for( i in 0... image.width ){
+            for( j in 0... image.height ){
+                var height=image.getPixel(i,j)&0xFF;
+                var heat:Float;
+                heat=4*j*(image.height-j)/(image.height*image.height);
+                ret.setPixel(i,j,(0xFF-height)/*blue*/+ ((Std.int(Math.max((Std.int(heat*0xFF)-Std.int(height/4)),0))&0xFF)<<16));
+            }
+        }
+        ret.unlock();
+        return ret;
 	}
 }
